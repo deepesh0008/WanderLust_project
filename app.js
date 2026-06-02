2e389b181bf029d0afd90a4d37932e799029049a
Deepesh <deepeshkumar7014@gmail.com>
2026-06-02 11:29:53 +0530

    fix root route redirect

--- app.js
+++ app.js
@@ -71,9 +71,9 @@ const sessionOptions = {
     }
 };
 
-// app.get("/" , (req,res) => {
-//     res.send("HI , I am Root")
-// });
+app.get("/" , (req,res) => {
+    res.redirect("/listings");
+});
 
 app.use(session(sessionOptions));
 app.use(flash());
